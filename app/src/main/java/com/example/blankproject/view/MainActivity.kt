a0bc0b92b1c65fea31ec22b11416ed39fc830d60
Gallois <premiersoft-gabriel.ficht@ailos.coop.br>
2021-07-16 12:41:07 -0300

    [ADD] Base Activity

--- app/src/main/java/com/example/blankproject/view/MainActivity.kt
+++ app/src/main/java/com/example/blankproject/view/MainActivity.kt
@@ -2,23 +2,20 @@ package com.example.blankproject.view
 
 import android.os.Bundle
 import android.os.PersistableBundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.example.blankproject.R
 import com.example.blankproject.databinding.ActivityMainBinding
+import com.example.blankproject.view.base.BaseActivity
 
-class MainActivity: AppCompatActivity(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     private val navController: NavController by lazy {
         findNavController(R.id.nav_host_fragment)
     }
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
     }
 }
